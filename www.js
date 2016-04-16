4b646d32ae0ae88f863874c85d0dd7be17e08851
dha <dha01@yandex.ru>
2016-04-16 16:10:58 +0500

    Асинхронная работа с потоками

--- www.js
+++ www.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var net = require('net');
+var streamm = require( "stream" );
 
 var ip_address = '192.168.1.64';
 var port = 1234;
 
-var file_name = '2.mp4';
+var remote_port = 1235;
 
-// Максимальнй размер одного сообщения.
-var max_size = 65536;
+var send_file_name = '1.jpg';
+var receive_file_name = 'receive.jpg'
 
 // Принять файл.
 var ReceiveFile = 1;
@@ -19,35 +20,46 @@ var file_count = 0;
 
 require('net').createServer(function (socket) {
     console.log("connected");
-    var pos = 0;
+    var remote_ip = socket.remoteAddress;
+
     var cur_id = file_count;
+    file_count++;
+
     // Генерируем уникальное имя для файла.
     var tmp_file_name = 'recive_file_' + cur_id;
-    var fd = fs.openSync(tmp_file_name, 'w');
-    file_count++;
     var message_type = -1;
 
-    // Получаем файл.
-    socket.on('data', function (data) {
-        if(pos == 0){
-            message_type = data[0];
-            fs.writeSync(fd, data, 1, data.byteLength - 1, pos);
-            pos += data.byteLength - 1;
+    console.log(tmp_file_name);
+
+    var writable = fs.createWriteStream(tmp_file_name);
+
+    function wait_first_block( data ) {
+        var bufferStream = new streamm.PassThrough();
+        if(message_type == -1)
+        {
+            message_type = data.slice(0,1)[0];
+            bufferStream.write(data.slice(1));
+            console.log('type = ', message_type);
         }
         else
         {
-            fs.writeSync(fd, data, 0, data.byteLength, pos);
-            pos += data.byteLength;
+            bufferStream.write(data);
         }
-        //console.log(cur_id + ' ' + pos);
-    });
+        bufferStream.pipe(writable).setMaxListeners(20);
+    }
 
+    socket.on('data',wait_first_block);
+
+    // Файл был получен.
     socket.on('end', function() {
         console.log('Получен файл.');
-        fs.closeSync(fd);
+        var bufferStream = new streamm.PassThrough();
+        bufferStream.end();
+        bufferStream.pipe(writable);
 
         if(message_type == ReveiveFileAndExecProc) {
-            ExecProcAndSendFile(tmp_file_name, socket.remoteAddress);
+            ExecProcAndSendFile(tmp_file_name, remote_ip);
+            //fs.unlink(file_name);
         }
     });
 }).listen(port);
@@ -56,45 +68,30 @@ require('net').createServer(function (socket) {
 function ExecProcAndSendFile(file_name, ip_address){
     const exec = require('child_process').exec;
     var copy_file_name = file_name + '_copy';
+    console.log('Copy ' + file_name);
     exec('CopyPaster.exe ' + file_name + ' ' + copy_file_name, function (err, stdout, stderr) {
+        // console.log(err);
         // Возвращаем результат выполнения по указанному IP-адресу.
-        Send(ip_address, port, copy_file_name, ReceiveFile);
+        Send(ip_address, remote_port, copy_file_name, ReceiveFile);
         console.log('Выполнена подпрограмма и результат отправлен по адресу ');
+        //fs.unlink(copy_file_name);
     });
 }
 
 // Отправляет файл по указанному адресу.
 function Send(ip, port, file_name, type) {
-    // Открываем файл для чтения.
-    fd = fs.openSync(file_name, 'r');
-
-    // Устанавливаем соединение через сокет.
-    var client = net.connect(port, ip);
-
-    var s_buffer = new Buffer(1);
-    s_buffer[0] = type;
-    // Отправляем тип сообщения.
-    client.write(s_buffer);
-
-    var buffer = new Buffer(max_size);
-    var pos = 0;
-
-    // Отправляем файл блоками размером MAX_PART_SIZE байт, до тех пор пока не будет считан весь файл.
-    while (true) {
-        var size = fs.readSync(fd, buffer, 0, max_size, pos);
-
-        if (size == 0) { break; }
-
-        pos += size;
-        var send_buffer = new Buffer(size);
-        buffer.copy(send_buffer, 0, 0, size);
-        client.write(send_buffer);
+    var file = fs.createReadStream(file_name);
+    var client = net.connect({ host: ip, port: port }, connected);
+    console.log(ip + ':' + port);
+    function connected() {
+        var bufferStream = new streamm.PassThrough();
+        var data = new Buffer(1);
+        data[0] = type;
+        bufferStream.write(data);
+        bufferStream.pipe(client);
+        file.pipe(client);
     }
-    fs.closeSync(fd);
-
-    // Закрываем сокет.
-    client.end();
 }
 
 // Отправляем файл.
-Send(ip_address, port, file_name, ReveiveFileAndExecProc);
+//Send(ip_address, port, send_file_name, ReveiveFileAndExecProc);
